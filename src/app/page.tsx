d7bde7f92ccf80c33321a4589dba132a65bdb9b0
Sarthak Panda <pandasarthak12345@gmail.com>
2024-07-28 18:13:34 +0530

    Project Complete
    Project Complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
-
+import HeroSection from "@/components/HeroSection"
+import FeaturedCourses from "@/components/FeaturedCourses"
+import WhyChooseUs from "@/components/WhyChooseUs";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import MeetOurInstructors from "@/components/MeetOurInstructors";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-    Home
-  </main>
+      <HeroSection/>
+      <FeaturedCourses />
+      <WhyChooseUs/>
+      <TestimonialCards/>
+      <UpcomingWebinars/>
+      <MeetOurInstructors/>
+    </main>
   );
 }
